93afc6793891958699632eda1e8cec345c1d4715
KNBrgb <175444883+KNBrgb@users.noreply.github.com>
2024-08-16 17:15:31 -0400

    added css objs and tested css var

---
+++ DOM_Manipulation_SBA/src/game.js
@@ -0,0 +1,11 @@
+// function startGame() {
+//     let currentNum = Math.floor(Math.random()*7) + 1;
+
+//     for(let i=0; i<5; i++){
+//     let holeContainer = document.getElementsByClassName('.holeContainer');
+
+// document.holeContainer.style.backgroundColor = ('var(--main-bg)')
+
+//}};
+const gardenColor = document.querySelector(".garden");
+gardenColor.style.backgroundColor = 'var(--main-bg)';
