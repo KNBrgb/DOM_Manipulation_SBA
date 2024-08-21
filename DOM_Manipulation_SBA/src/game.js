b05c75817874a8a1ecde8a15bce7e091882f0a0d
KNBrgb <175444883+KNBrgb@users.noreply.github.com>
2024-08-20 23:16:32 -0400

    working better

--- DOM_Manipulation_SBA/src/game.js
+++ DOM_Manipulation_SBA/src/game.js
@@ -1,15 +1,3 @@
-// function startGame() {
-//     let currentNum = Math.floor(Math.random()*7) + 1;
-
-//     for(let i=0; i<5; i++){
-//     let holeContainer = document.getElementsByClassName('.holeContainer');
-
-// document.holeContainer.style.backgroundColor = ('var(--main-bg)')
-
-//}};
-//
-// gardenColor.style.backgroundColor = 'var(--main-bg)';
-
 const answerEl = document.querySelector("#answer");
 const winEl = document.getElementById("win");
 const lossEl = document.getElementById("loss");
@@ -17,25 +5,25 @@ const whackButton = document.getElementById("whack");
 const resetButton = document.getElementById("resetButton");
 const guessForm = document.getElementById("guessForm");
 const guessInput = document.getElementById("guessInput");
-// const gardenColor = document.querySelector(".garden");
 
 let currentNumber;
 let tries;
 
 function startGame() {
-  // let currentNumber = 1
   currentNumber = (Math.floor(Math.random() * 7) + 1).toString();
   tries = 3;
   resetButton.style.display = "none";
   guessInput.value = "";
   answerEl.innerHTML = "";
   guessInput.style.display = "block";
-  guessForm.style.display = '';
-  whackButton.style.display = '';
+  // guessForm.style.display = '';
+  guessInput.focus();
 }
 
 function nextTry() {
   const tryGuess = guessInput.value;
+  // document.body.innerHTML.querySelector("#guessInput");
+  // guessInput.style.display = 'block'
 
   if (!/^[1-7]$/.test(tryGuess)) {
     alert("Your guess should only include a number between 1 and 7.");
@@ -47,13 +35,13 @@ function nextTry() {
     document.body.style.backgroundColor = "var(--winner-bg)";
     winEl.style.display = "block";
     answerEl.innerHTML = "Holy moly, you got 'em!";
-    resetButton.style.display = "block";
-    guessForm.style.display = 'none';
-    
+    resetButton.style.display = "block"; 
   } else {
     tries--;
     if (tries > 0) {
       answerEl.innerHTML = `Try again! You have ${tries} tries left.`;
+      guessInput.style.display = 'block';
+
     } else {
       document.body.style.backgroundColor = "var(--loser-bg)";
       answerEl.style.color = "red";
@@ -61,9 +49,7 @@ function nextTry() {
       resetButton.style.display = "block";
       lossEl.style.display = "block";
       guessInput.style.display = 'none';
-      whackButton.style.display = 'none';
-      guessForm.style.display = 'none';
-      resetButton.style.display = 'block';
+      // guessForm.style.display = 'none';
       
 
       // ADD A BREAK TO STOP THE LOOP, THEN AND FUNCTION TO BUTTON TO RESTART (button = startGame)
@@ -86,13 +72,13 @@ function resetGame() {
   startGame();
 }
 
-whackButton.addEventListener("click", startGame);
-resetButton.addEventListener("click", resetGame);
 
 guessForm.addEventListener('submit', function(event) {
-  event.preventDefault();
+  // event.preventDefault();
   nextTry();
 });
+
+startGame();
 
 // resetButton.style.display = "none";
 
